1d814901d8c17eec00f8cde043305b720b31ed9e
johnwheeler <john@johnwheeler.org>
2017-07-28 11:52:49 -0700

    update bmx to bmx-snapshot-20170117.tar.gz

--- libmxfpp.rb
+++ libmxfpp.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Libmxfpp < Formula
   homepage "http://sourceforge.net/p/bmxlib/home/Home/"
-  url "https://downloads.sourceforge.net/project/bmxlib/bmx-snapshot-20150603/bmx-snapshot-20150603.tar.gz"
-  sha256 "89ee63ce06a2e0e3d512bbd0cf9b37b9c8b3bc1e4b3685e14d6bfddd29527d30"
+  url "https://downloads.sourceforge.net/project/bmxlib/bmx-snapshot-20170117/bmx-snapshot-20170117.tar.gz"
+  sha256 "fdaaf43f60f916c76c2c9b956c05466ac41f5dc23a926c258611acfb7d557c7a"
 
   depends_on "pkg-config" => :build
   depends_on "libtool" => :build
